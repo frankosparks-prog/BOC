8728bf84054f67657ae945fa9cdcc42423408dcf
frankosparks-prog <mainafrank400@gmail.com>
2026-02-03 19:58:47 +0300

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,11 +22,16 @@ module.exports = {
       },
       animation: {
         'float': 'float 6s ease-in-out infinite',
+        'marquee': 'marquee 25s linear infinite',
       },
       keyframes: {
         float: {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(-20px)' },
+        },
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
         }
       }
     },
